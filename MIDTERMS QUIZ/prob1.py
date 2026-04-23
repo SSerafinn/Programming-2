9fd010663b659d38d15822e71b5d5100f2da4fed
Sera_Dawgs <jaypeegazz@gmail.com>
2026-04-23 14:21:01 +0800

    SAGOT SA QUIZ

---
+++ MIDTERMS QUIZ/prob1.py
@@ -0,0 +1,7 @@
+food_dict = {
+    "fruit":["banana","apple","orange"],
+    "vegetables":["carrot","celery","broccoli"],
+    "meat":["chicken","beef","pork"]
+}
+
+searchKey = input("Enter the key: ").lower()
